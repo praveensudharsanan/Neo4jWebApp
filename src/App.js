01c9e6c8ff1b6dd2e4dd41a4ffbd350155397006
Praveen S <praveensudharsanan@gmail.com>
2023-07-04 13:51:46 -0400

    Added the visualization view with backend api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUniversity } from '@fortawesome/free-solid-svg-icons';
 import Mainscreen from './Mainscreen';
-import GraphView from './Visualization';
+import Neo4jVisualization from './Visualization';
 const App = () => {
   return (
     <div>
@@ -11,7 +11,7 @@ const App = () => {
         &nbsp;University of Liverpool
       </h1>
       <Mainscreen />
-    <GraphView/>
+    <Neo4jVisualization/>
     </div>
     
   );
--- src/Visualization.js
+++ src/Visualization.js
@@ -1,67 +1,94 @@
-import React from 'react'
-import Graph from 'react-vis-network-graph'
+import React, { useEffect, useState } from 'react';
+import Graph from 'react-graph-vis';
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
-export default function GraphView() {
-    const graph = {
-        nodes: [
-            {id: 1, label: "Node 1", title: "node 1 tooltip text",
-            shape: "box"},
-            {id: 2, label: "Node 2", title: "node 2 tooltip text",
-            shape: "circle"},
-            {id: 3, label: "Node 3", title: "node 3 tooltip text",
-            shape: "diamond"},
-            {id: 4, label: "Node 4", title: "node 4 tooltip text",
-            shape: "star"},
-            {id: 5, label: "Node 5", title: "node 5 tooltip text"},
-            {id: 6, label: "Node 6", title: "node 6 tooltip text"},
-            {id: 7, label: "Node 7", title: "node 7 tooltip text"},
-            {id: 8, label: "Node 8", title: "node 8 tooltip text"},
-            {id: 9, label: "Node 9", title: "node 9 tooltip text"}
-        ],
-        edges: [
-            {from: 1, to: 1, smooth: {type: "curvedCW"}, arrows: {from: {enabled: true, type: "circle"}, to: {enabled: true, type: "circle"}}},
-            {from: 1, to: 7, arrows: {from: {enabled: true, type: "vee"}, to: {enabled: true, type: "vee"}}},
-            {from: 1, to: 3, arrows: {to: {enabled: true, type: "curve"}}},
-            {from: 6, to: 5, color: {highlight: "#fff", opacity: 0.2}},
-            {from: 6, to: 2},
-            {from: 7, to: 2},
-            {from: 6, to: 7},
-            {from: 6, to: 8},
-            {from: 7, to: 8},
-            {from: 8, to: 2},
-            {from: 3, to: 7},
-        ]
-    }
+const Neo4jVisualization = () => {
+  const [graphData, setGraphData] = useState({ nodes: [], edges: [] });
+  const [graphKey, setGraphKey] = useState(uuidv4());
 
-    var options = {
-        physics: {
-            enabled: false
-        },
-        interaction: {
-            navigationButtons: true
-        },
-        nodes: {
-            borderWidth: 2,
-            size: 40,
-            color: {
-                border: "#222222",
-                background: "#666666"
-            },
-            font: {color: "yellow"}
-        },
-        edges: {
-            color: "yellow"
-        },
-        shadow: true,
-        smooth: true,
-        height: "900px"
-    }
+  useEffect(() => {
+    const fetchDataFromApi = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/test_api/neo4j_get/');
+        const data = response.data.result;
+
+        console.log('API Response:', data);
+
+        if (!data) {
+          console.error('Invalid API response:', response.data);
+          return;
+        }
+
+        const nodes = data.map((node) => {
+          const nodeId = node.identity.low.toString();
+          const nodeLabels = node.labels;
+          const nodeProperties = node.properties;
+
+          return {
+            id: nodeId,
+            label: nodeLabels.join(', '), // Concatenate all labels
+            title: JSON.stringify(nodeProperties), // Display properties as a JSON string
+          };
+        });
+
+        setGraphData({ nodes, edges: [] });
+        setGraphKey(uuidv4()); // Generate a new key
+      } catch (error) {
+        console.error('Error fetching data from API:', error);
+      }
+    };
+
+    fetchDataFromApi();
+  }, []);
+
+  console.log('Graph Data:', graphData);
+
+  const options = {
+    layout: {
+      hierarchical: false, // Disable hierarchical layout
+    },
+    nodes: {
+      shape: 'circle', // Use circle shape for nodes
+      font: {
+        size: 16, // Font size for node labels
+      },
+    },
+    edges: {
+      color: '#000000', // Edge color
+      width: 1, // Edge width
+      arrows: {
+        to: { enabled: true, scaleFactor: 0.5 }, // Enable arrow at the end of edges
+      },
+    },
+    physics: {
+      enabled: true, // Enable physics simulation
+      solver: 'forceAtlas2Based', // Use forceAtlas2 algorithm for layout
+      forceAtlas2Based: {
+        gravitationalConstant: -300, // Attraction force between nodes
+        centralGravity: 0.02, // Strength of the central gravity force
+        springLength: 100, // Length of the edges
+        springConstant: 0.08, // Strength of the spring forces
+        avoidOverlap: 1, // Avoid node overlap
+      },
+      maxVelocity: 146, // Maximum node velocity
+      timestep: 0.35, // Simulation timestep
+      stabilization: {
+        enabled: true, // Enable stabilization of the graph after initialization
+        iterations: 200, // Number of stabilization iterations
+        updateInterval: 10, // Update stabilization state every 10 milliseconds
+        onlyDynamicEdges: false, // Include static edges in stabilization
+        fit: true, // Fit the graph after stabilization
+      },
+    },
+  };
+
   return (
-    <div className='container'>
-        <Graph 
-            graph={graph}
-            options={options}
-        />
+    <div>
+      <h2>Neo4j Visualization</h2>
+      <Graph key={graphKey} graph={graphData} options={options} style={{ height: '500px' }} />
     </div>
-  )
-}
+  );
+};
+
+export default Neo4jVisualization;
